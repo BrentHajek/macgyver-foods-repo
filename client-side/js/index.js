3ebdd56bed069913278535d4bf65ee0f5fea0356
leepowe <leepowell2@yahoo.com>
2021-07-26 15:42:31 -0400

    waiting for nav to test some functionality

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,2 +1,19 @@
-const container = document.querySelector('.container');
-container.innerText = 'hello world!';
+import LogInPage from './pages/LogInPage';
+const container = document.querySelector('.container'),
+const LogInButton = document.querySelector('.LogIn__Form_Button');
+let LogIns = [
+    {
+        username: 'leepowe', 
+        password: 'NoPassword'
+    }
+]
+function getInfo(){
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    console.log('your username  is ' + username + ' and your password is ' + password);
+    for(i = 0; i < LogIns.length; i++) {
+        if(username == LogIns[i].username && password == LogIns[i].password) {
+            console.log(username + ' is logged in!')
+        }
+    }
+}
---
+++ client-side/js/pages/LogInPage.js
@@ -0,0 +1,9 @@
+export default function LogInPage() {
+    return `
+      <form class="LogIn__Form>
+          <input id="username">
+          <input id="password">
+          <button class="LogIn__Form_Button" type="button">Submit</button>
+      </form>
+      `;
+  }
