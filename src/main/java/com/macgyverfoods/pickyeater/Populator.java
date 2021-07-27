3ad6e4dc589c3cfbc0bf9daad3731ee26f1ba63b
shuaybfarah <shuaybfarah18@gmail.com>
2021-07-27 08:56:34 -0400

    started to populate the populator, I added an add/remove method to the parent model and tested it. It works

--- src/main/java/com/macgyverfoods/pickyeater/Populator.java
+++ src/main/java/com/macgyverfoods/pickyeater/Populator.java
@@ -1,6 +1,7 @@
 package com.macgyverfoods.pickyeater;
 
 import com.macgyverfoods.pickyeater.models.FoodCategory;
+import com.macgyverfoods.pickyeater.models.Ingredient;
 import com.macgyverfoods.pickyeater.repositories.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -45,8 +46,67 @@ public class Populator implements CommandLineRunner {
         foodCategoryRepo.save(protein);
         foodCategoryRepo.save(vegetables);
 
-        
+        Ingredient butter = new Ingredient("Butter",dairy);
+        Ingredient cheese = new Ingredient("Cheese",dairy);
+        Ingredient cream = new Ingredient("Cream",dairy);
+        Ingredient iceCream = new Ingredient("Ice Cream",dairy);
+        Ingredient milk = new Ingredient("Milk",dairy);
+        Ingredient yogurt = new Ingredient("Yogurt",dairy);
+        ingredientRepo.save(butter);
+        ingredientRepo.save(cheese);
+        ingredientRepo.save(cream);
+        ingredientRepo.save(iceCream);
+        ingredientRepo.save(milk);
+        ingredientRepo.save(yogurt);
 
+        Ingredient apple = new Ingredient("Apple",fruits);
+        Ingredient avocado = new Ingredient("Avocado",fruits);
+        Ingredient banana = new Ingredient("Banana",fruits);
+        Ingredient mango = new Ingredient("Mango",fruits);
+        Ingredient pear = new Ingredient("Pear",fruits);
+        Ingredient pineapple = new Ingredient("Pineapple",fruits);
+        Ingredient orange = new Ingredient("Orange",fruits);
+        Ingredient strawberry = new Ingredient("Strawberry",fruits);
+        Ingredient tomato = new Ingredient("Tomato",fruits);
+        Ingredient watermelon = new Ingredient("Watermelon",fruits);
+        ingredientRepo.save(apple);
+        ingredientRepo.save(avocado);
+        ingredientRepo.save(banana);
+        ingredientRepo.save(mango);
+        ingredientRepo.save(pear);
+        ingredientRepo.save(pineapple);
+        ingredientRepo.save(orange);
+        ingredientRepo.save(strawberry);
+        ingredientRepo.save(tomato);
+        ingredientRepo.save(watermelon);
+
+        Ingredient bread = new Ingredient("Bread",grains);
+        Ingredient cereal = new Ingredient("Cereal",grains);
+        Ingredient corn = new Ingredient("Corn",grains);
+        Ingredient pasta = new Ingredient("Pasta",grains);
+        Ingredient oats = new Ingredient("Oats",grains);
+        Ingredient rice = new Ingredient("Rice",grains);
+        ingredientRepo.save(bread);
+        ingredientRepo.save(cereal);
+        ingredientRepo.save(corn);
+        ingredientRepo.save(pasta);
+        ingredientRepo.save(oats);
+        ingredientRepo.save(rice);
+
+        Ingredient beef = new Ingredient("Beef",protein);
+        Ingredient chicken = new Ingredient("Chicken",protein);
+        Ingredient eggs = new Ingredient("Beef",protein);
+        Ingredient fish = new Ingredient("Fish",protein);
+        Ingredient lamb = new Ingredient("Lamb",protein);
+        Ingredient shrimp = new Ingredient("Shrimp",protein);
+        Ingredient turkey = new Ingredient("Turkey",protein);
+        ingredientRepo.save(beef);
+        ingredientRepo.save(chicken);
+        ingredientRepo.save(eggs);
+        ingredientRepo.save(fish);
+        ingredientRepo.save(lamb);
+        ingredientRepo.save(shrimp);
+        ingredientRepo.save(turkey);
 
     }
 }
--- src/test/java/com/macgyverfoods/pickyeater/PickyEaterTests.java
+++ src/test/java/com/macgyverfoods/pickyeater/PickyEaterTests.java
@@ -145,8 +145,10 @@ public class PickyEaterTests {
         childRepo.save(testChild2);
         childRepo.save(testChild3);
 
-        Parent testParent1 = new Parent("John","Doe","JohnDoe18@gmail.com",testIngredient,testIngredient2);
-        Parent testParent2 = new Parent("Test","Subject","TestSubject@gmail.com",testIngredient3,testIngredient4);
+        Parent testParent1 = new Parent("John","Doe","JohnDoe18@gmail.com");
+//        ,testIngredient,testIngredient2
+        Parent testParent2 = new Parent("Test","Subject","TestSubject@gmail.com");
+//        ,testIngredient3,testIngredient4
         parentRepo.save(testParent1);
         parentRepo.save(testParent2);
 
@@ -219,8 +221,10 @@ public class PickyEaterTests {
         childRepo.save(testChild4);
         childRepo.save(testChild5);
 
-        Parent testParent1 = new Parent("John","Doe","JohnDoe18@gmail.com",testIngredient,testIngredient2);
-        Parent testParent2 = new Parent("Test","Subject","TestSubject@gmail.com",testIngredient3,testIngredient4);
+        Parent testParent1 = new Parent("John","Doe","JohnDoe18@gmail.com");
+//        ,testIngredient,testIngredient2
+        Parent testParent2 = new Parent("Test","Subject","TestSubject@gmail.com");
+//        ,testIngredient3,testIngredient4
         parentRepo.save(testParent1);
         parentRepo.save(testParent2);
 
@@ -244,6 +248,258 @@ public class PickyEaterTests {
         assertThat(retrievedParent1.getChild()).contains(testChild,testChild2,testChild5);
         assertThat(retrievedParent2.getChild()).contains(testChild3,testChild4);
 
+    }
+
+    @Test
+    public void shouldBeAbleToRemoveChildFromParent(){
+
+        FoodCategory testFoodCategory = new FoodCategory("Dairy","dairy.jpg");
+        FoodCategory testFoodCategory2 = new FoodCategory("Meat","meat.jpg");
+        foodCategoryRepo.save(testFoodCategory);
+        foodCategoryRepo.save(testFoodCategory2);
+
+        Ingredient testIngredient = new Ingredient("milk1",testFoodCategory);
+        Ingredient testIngredient2 = new Ingredient("Milk2",testFoodCategory);
+        Ingredient testIngredient3 = new Ingredient("fish",testFoodCategory2);
+        Ingredient testIngredient4 = new Ingredient("chicken",testFoodCategory2);
+        ingredientRepo.save(testIngredient);
+        ingredientRepo.save(testIngredient2);
+        ingredientRepo.save(testIngredient3);
+        ingredientRepo.save(testIngredient4);
+
+        Preference testPreference = new Preference(testIngredient);
+        Preference testPreference2 = new Preference(testIngredient2);
+        Preference testPreference3 = new Preference(testIngredient3);
+        Preference testPreference4 = new Preference(testIngredient4);
+        preferenceRepo.save(testPreference);
+        preferenceRepo.save(testPreference2);
+        preferenceRepo.save(testPreference3);
+        preferenceRepo.save(testPreference4);
+
+        Allergy testAllergy = new Allergy("Lactose Tolerant");
+        Allergy testAllergy2 = new Allergy("Peanut Allergy");
+        Allergy testAllergy3 = new Allergy("Pollen");
+        Allergy testAllergy4 = new Allergy("Fur");
+        allergyRepo.save(testAllergy);
+        allergyRepo.save(testAllergy2);
+        allergyRepo.save(testAllergy3);
+        allergyRepo.save(testAllergy4);
+
+        SubChild testSubChild = new SubChild("1","2","3",testAllergy,testAllergy3);
+        SubChild testSubChild2 = new SubChild("1","2","3",testAllergy4,testAllergy3);
+        SubChild testSubChild3 = new SubChild("1","2","3",testAllergy2);
+        subChildRepo.save(testSubChild);
+        subChildRepo.save(testSubChild2);
+        subChildRepo.save(testSubChild3);
+
+        Child testChild = new Child(testSubChild,testPreference,testPreference2,testPreference3);
+        Child testChild2 = new Child(testSubChild2,testPreference,testPreference2);
+        Child testChild3 = new Child(testSubChild3,testPreference4);
+        Child testChild4 = new Child(testSubChild3,testPreference,testPreference2,testPreference3,testPreference4);
+        Child testChild5 = new Child(testSubChild3,testPreference);
+        childRepo.save(testChild);
+        childRepo.save(testChild2);
+        childRepo.save(testChild3);
+        childRepo.save(testChild4);
+        childRepo.save(testChild5);
+
+        Parent testParent1 = new Parent("John","Doe","JohnDoe18@gmail.com");
+        Parent testParent2 = new Parent("Test","Subject","TestSubject@gmail.com");
+        parentRepo.save(testParent1);
+        parentRepo.save(testParent2);
+
+        testParent1.addChild(testChild);
+        testParent1.addChild(testChild2);
+        testParent1.addChild(testChild5);
+        testParent2.addChild(testChild3);
+        testParent2.addChild(testChild4);
+
+        testParent2.removeChild(testChild5);
+
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Parent retrievedParent1 = parentRepo.findById(testParent1.getId()).get();
+        Parent retrievedParent2 = parentRepo.findById(testParent2.getId()).get();
+        Child retrievedChild1 = childRepo.findById(testChild.getId()).get();
+        Child retrievedChild2 = childRepo.findById(testChild2.getId()).get();
+        Child retrievedChild3 = childRepo.findById(testChild3.getId()).get();
+        Child retrievedChild4 = childRepo.findById(testChild4.getId()).get();
+        Child retrievedChild5 = childRepo.findById(testChild5.getId()).get();
+        assertThat(retrievedParent1.getChild()).contains(testChild,testChild2);
+        assertThat(retrievedParent2.getChild()).contains(testChild3,testChild4);
+    }
+
+    @Test
+    public void shouldAddIngredientToParent() {
+
+        FoodCategory testFoodCategory = new FoodCategory("Dairy","dairy.jpg");
+        FoodCategory testFoodCategory2 = new FoodCategory("Meat","meat.jpg");
+        foodCategoryRepo.save(testFoodCategory);
+        foodCategoryRepo.save(testFoodCategory2);
+
+        Ingredient testIngredient = new Ingredient("milk1",testFoodCategory);
+        Ingredient testIngredient2 = new Ingredient("Milk2",testFoodCategory);
+        Ingredient testIngredient3 = new Ingredient("fish",testFoodCategory2);
+        Ingredient testIngredient4 = new Ingredient("chicken",testFoodCategory2);
+        ingredientRepo.save(testIngredient);
+        ingredientRepo.save(testIngredient2);
+        ingredientRepo.save(testIngredient3);
+        ingredientRepo.save(testIngredient4);
+
+        Preference testPreference = new Preference(testIngredient);
+        Preference testPreference2 = new Preference(testIngredient2);
+        Preference testPreference3 = new Preference(testIngredient3);
+        Preference testPreference4 = new Preference(testIngredient4);
+        preferenceRepo.save(testPreference);
+        preferenceRepo.save(testPreference2);
+        preferenceRepo.save(testPreference3);
+        preferenceRepo.save(testPreference4);
+
+        Allergy testAllergy = new Allergy("Lactose Tolerant");
+        Allergy testAllergy2 = new Allergy("Peanut Allergy");
+        Allergy testAllergy3 = new Allergy("Pollen");
+        Allergy testAllergy4 = new Allergy("Fur");
+        allergyRepo.save(testAllergy);
+        allergyRepo.save(testAllergy2);
+        allergyRepo.save(testAllergy3);
+        allergyRepo.save(testAllergy4);
+
+        SubChild testSubChild = new SubChild("1","2","3",testAllergy,testAllergy3);
+        SubChild testSubChild2 = new SubChild("1","2","3",testAllergy4,testAllergy3);
+        SubChild testSubChild3 = new SubChild("1","2","3",testAllergy2);
+        subChildRepo.save(testSubChild);
+        subChildRepo.save(testSubChild2);
+        subChildRepo.save(testSubChild3);
+
+        Child testChild = new Child(testSubChild,testPreference,testPreference2,testPreference3);
+        Child testChild2 = new Child(testSubChild2,testPreference,testPreference2);
+        Child testChild3 = new Child(testSubChild3,testPreference4);
+        Child testChild4 = new Child(testSubChild3,testPreference,testPreference2,testPreference3,testPreference4);
+        Child testChild5 = new Child(testSubChild3,testPreference);
+        childRepo.save(testChild);
+        childRepo.save(testChild2);
+        childRepo.save(testChild3);
+        childRepo.save(testChild4);
+        childRepo.save(testChild5);
+
+        Parent testParent1 = new Parent("John","Doe","JohnDoe18@gmail.com");
+        Parent testParent2 = new Parent("Test","Subject","TestSubject@gmail.com");
+        parentRepo.save(testParent1);
+        parentRepo.save(testParent2);
+
+        testParent1.addChild(testChild);
+        testParent1.addChild(testChild2);
+        testParent1.addChild(testChild5);
+        testParent2.addChild(testChild3);
+        testParent2.addChild(testChild4);
+
+        testParent2.removeChild(testChild5);
+
+        testParent1.addIngredient(testIngredient);
+        testParent1.addIngredient(testIngredient2);
+        testParent2.addIngredient(testIngredient3);
+        testParent2.addIngredient(testIngredient4);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Parent retrievedParent1 = parentRepo.findById(testParent1.getId()).get();
+        Parent retrievedParent2 = parentRepo.findById(testParent2.getId()).get();
+        Ingredient retrievedIngredient1 = ingredientRepo.findById(testIngredient.getId()).get();
+        Ingredient retrievedIngredient2 = ingredientRepo.findById(testIngredient2.getId()).get();
+        Ingredient retrievedIngredient3 = ingredientRepo.findById(testIngredient3.getId()).get();
+        Ingredient retrievedIngredient4 = ingredientRepo.findById(testIngredient4.getId()).get();
+        assertThat(retrievedParent1.getIngredients()).contains(testIngredient,testIngredient2);
+        assertThat(retrievedParent2.getIngredients()).contains(testIngredient3,testIngredient4);
+    }
+
+    @Test
+    public void shouldRemoveIngredientFromParent() {
+
+        FoodCategory testFoodCategory = new FoodCategory("Dairy","dairy.jpg");
+        FoodCategory testFoodCategory2 = new FoodCategory("Meat","meat.jpg");
+        foodCategoryRepo.save(testFoodCategory);
+        foodCategoryRepo.save(testFoodCategory2);
+
+        Ingredient testIngredient = new Ingredient("milk1",testFoodCategory);
+        Ingredient testIngredient2 = new Ingredient("Milk2",testFoodCategory);
+        Ingredient testIngredient3 = new Ingredient("fish",testFoodCategory2);
+        Ingredient testIngredient4 = new Ingredient("chicken",testFoodCategory2);
+        ingredientRepo.save(testIngredient);
+        ingredientRepo.save(testIngredient2);
+        ingredientRepo.save(testIngredient3);
+        ingredientRepo.save(testIngredient4);
+
+        Preference testPreference = new Preference(testIngredient);
+        Preference testPreference2 = new Preference(testIngredient2);
+        Preference testPreference3 = new Preference(testIngredient3);
+        Preference testPreference4 = new Preference(testIngredient4);
+        preferenceRepo.save(testPreference);
+        preferenceRepo.save(testPreference2);
+        preferenceRepo.save(testPreference3);
+        preferenceRepo.save(testPreference4);
+
+        Allergy testAllergy = new Allergy("Lactose Tolerant");
+        Allergy testAllergy2 = new Allergy("Peanut Allergy");
+        Allergy testAllergy3 = new Allergy("Pollen");
+        Allergy testAllergy4 = new Allergy("Fur");
+        allergyRepo.save(testAllergy);
+        allergyRepo.save(testAllergy2);
+        allergyRepo.save(testAllergy3);
+        allergyRepo.save(testAllergy4);
+
+        SubChild testSubChild = new SubChild("1","2","3",testAllergy,testAllergy3);
+        SubChild testSubChild2 = new SubChild("1","2","3",testAllergy4,testAllergy3);
+        SubChild testSubChild3 = new SubChild("1","2","3",testAllergy2);
+        subChildRepo.save(testSubChild);
+        subChildRepo.save(testSubChild2);
+        subChildRepo.save(testSubChild3);
+
+        Child testChild = new Child(testSubChild,testPreference,testPreference2,testPreference3);
+        Child testChild2 = new Child(testSubChild2,testPreference,testPreference2);
+        Child testChild3 = new Child(testSubChild3,testPreference4);
+        Child testChild4 = new Child(testSubChild3,testPreference,testPreference2,testPreference3,testPreference4);
+        Child testChild5 = new Child(testSubChild3,testPreference);
+        childRepo.save(testChild);
+        childRepo.save(testChild2);
+        childRepo.save(testChild3);
+        childRepo.save(testChild4);
+        childRepo.save(testChild5);
+
+        Parent testParent1 = new Parent("John","Doe","JohnDoe18@gmail.com");
+        Parent testParent2 = new Parent("Test","Subject","TestSubject@gmail.com");
+        parentRepo.save(testParent1);
+        parentRepo.save(testParent2);
+
+        testParent1.addChild(testChild);
+        testParent1.addChild(testChild2);
+        testParent1.addChild(testChild5);
+        testParent2.addChild(testChild3);
+        testParent2.addChild(testChild4);
+
+        testParent2.removeChild(testChild5);
+
+        testParent1.addIngredient(testIngredient);
+        testParent1.addIngredient(testIngredient2);
+        testParent2.addIngredient(testIngredient3);
+        testParent2.addIngredient(testIngredient4);
+
+        testParent1.removeIngredient(testIngredient);
+        testParent2.removeIngredient(testIngredient3);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Parent retrievedParent1 = parentRepo.findById(testParent1.getId()).get();
+        Parent retrievedParent2 = parentRepo.findById(testParent2.getId()).get();
+        Ingredient retrievedIngredient1 = ingredientRepo.findById(testIngredient.getId()).get();
+        Ingredient retrievedIngredient2 = ingredientRepo.findById(testIngredient2.getId()).get();
+        Ingredient retrievedIngredient3 = ingredientRepo.findById(testIngredient3.getId()).get();
+        Ingredient retrievedIngredient4 = ingredientRepo.findById(testIngredient4.getId()).get();
+        assertThat(retrievedParent1.getIngredients()).contains(testIngredient2);
+        assertThat(retrievedParent2.getIngredients()).contains(testIngredient4);
     }
 
 }
