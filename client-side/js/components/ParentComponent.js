070d1912022ffd5fefa095d57880cbf110c49756
BrentHajek <brenthajek@hotmail.com>
2021-07-29 18:22:33 -0400

    revised branch

--- client-side/js/components/ParentComponent.js
+++ client-side/js/components/ParentComponent.js
@@ -2,9 +2,11 @@ export default function ParentComponent(parent) {
     return `
     <div class='parent__info_containter'>
         <input type='hidden' id='parentInfo' value=${parent.url} />
-        <h2 class='parent__name'>${parent.name}</h2>
+        <h2 class='parent__name_header'>Hello, ${parent.parentFirstName}</h2>
+        <li>First name: ${parent.parentFirstName}</li>
+        <li>Last name: ${parent.parentLastName}</li>
         <li>Email: ${parent.email}</li>
-        <li>List of preferences: ${parent.preferences}</li>
+        <li>Your preferences: ${parent.ingredients}</li>
     </div>
     `;
 }
