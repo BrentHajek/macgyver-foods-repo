cf7cdda5b888307abdf5aa9230ef2e09aec0979d
leepowe <leepowell2@yahoo.com>
2021-07-29 23:24:42 -0400

    modal now functioning still missing nav bar to open modal using as landing page currently

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,71 +1,93 @@
+const closeModalButtons = document.getElementById("close__button"),
+  overlay = document.getElementById("overlay");
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
 function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
-  
-    messageElement.textContent = message;
-    messageElement.classList.remove(
-      "form__message--success",
-      "form__message--error"
-    );
-    messageElement.classList.add(`form__message--${type}`);
-  }
-  
-  function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(
-      ".form__input-error-message"
-    ).textContent = message;
-  }
-  
-  function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(
-      ".form__input-error-message"
-    ).textContent = "";
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-  
-    document
-      .querySelector("#linkCreateAccount")
-      .addEventListener("click", (e) => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-      });
-  
-    document.querySelector("#linkLogin").addEventListener("click", (e) => {
+  const messageElement = formElement.querySelector(".form__message");
+
+  messageElement.textContent = message;
+  messageElement.classList.remove(
+    "form__message--success",
+    "form__message--error"
+  );
+  messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+  inputElement.classList.add("form__input--error");
+  inputElement.parentElement.querySelector(
+    ".form__input-error-message"
+  ).textContent = message;
+}
+
+function clearInputError(inputElement) {
+  inputElement.classList.remove("form__input--error");
+  inputElement.parentElement.querySelector(
+    ".form__input-error-message"
+  ).textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#createAccount");
+
+  document
+    .querySelector("#linkCreateAccount")
+    .addEventListener("click", (e) => {
       e.preventDefault();
-      loginForm.classList.remove("form--hidden");
-      createAccountForm.classList.add("form--hidden");
+      loginForm.classList.add("form--hidden");
+      createAccountForm.classList.remove("form--hidden");
     });
-  
-    loginForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-  
-      // Perform Fetch login
-  
-      setFormMessage(loginForm, "error", "Invalid username/password combination");
+
+  document.querySelector("#linkLogin").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
+  });
+
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Perform Fetch login
+
+    setFormMessage(loginForm, "error", "Invalid username/password combination");
+  });
+
+  document.querySelectorAll(".form__input").forEach((inputElement) => {
+    inputElement.addEventListener("blur", (e) => {
+      if (
+        e.target.id === "signupUsername" &&
+        e.target.value.length > 0 &&
+        e.target.value.length < 10
+      ) {
+        setInputError(
+          inputElement,
+          "Username must be at least 10 characters in length"
+        );
+      }
     });
-  
-    document.querySelectorAll(".form__input").forEach((inputElement) => {
-      inputElement.addEventListener("blur", (e) => {
-        if (
-          e.target.id === "signupUsername" &&
-          e.target.value.length > 0 &&
-          e.target.value.length < 10
-        ) {
-          setInputError(
-            inputElement,
-            "Username must be at least 10 characters in length"
-          );
-        }
-      });
-  
-      inputElement.addEventListener("input", (e) => {
-        clearInputError(inputElement);
-      });
+
+    inputElement.addEventListener("input", (e) => {
+      clearInputError(inputElement);
     });
   });
-  
+});
