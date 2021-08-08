c5d17684f85f9a3feffa6e5ebc2bf1e7921e8259
shuaybfarah <shuaybfarah18@gmail.com>
2021-08-08 12:33:31 -0400

    ingredients can now be added to the parent if those ingredients are listed in the populator

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -5,6 +5,7 @@ import AddChildPage from "./pages/AddChildPage.js"
 import DeleteChildPage from "./pages/DeleteChildPage.js"
 import RecipeInstructions from './components/RecipeInstructions.js';
 import RecipeIngredients from './components/RecipeIngredients.js';
+import AddIngredientPage from './pages/AddIngredientPage.js';
 
 
 buildPage();
@@ -16,8 +17,8 @@ function buildPage() {
 
 const app = document.querySelector('#app');
 
-const apiKeyNum = "78bc7509124db458df764b454c2dc1e57807eff5";
-// const apiKeyNum = "d16a986f5295496bb236ca7062f1841a";
+// const apiKeyNum = "78bc7509124db458df764b454c2dc1e57807eff5";
+const apiKeyNum = "d16a986f5295496bb236ca7062f1841a";
 // const apiKeyNum = "4d2f51bba03b42a59ba6d0843ac5b5f9";
 // const apiKeyNum = "733246d3691c4203855fd5063ee214b6";
 // const apiKeyNum = "00f757b09028492da86c30d8109241c0";
@@ -32,7 +33,9 @@ function renderProfileInfo() {
             navToDeleteChildPage();
             createChild();
             deleteChild();
+            AddIngredientToParent();
             renderRecipeInstructions();
+            navToAddIngredientPage();
         });
     });
 }
@@ -64,8 +67,9 @@ function createChild() {
             }, (parents) => {
                 console.log(parents);
                 app.innerHTML = ParentPage(parents);
-                navToAddChildPage();
                 navToDeleteChildPage();
+                navToAddChildPage();
+                navToAddIngredientPage();
             });
         }
     })
@@ -81,6 +85,7 @@ function deleteChild() {
                     app.innerHTML = ParentPage(parents)
                     navToDeleteChildPage();
                     navToAddChildPage();
+                    navToAddIngredientPage();
                 });
             }
         })
@@ -127,3 +132,26 @@ function renderRecipeInstructions() {
 //         })
 //     })
 // }
+
+function navToAddIngredientPage() {
+    const navToAddIngredientButton = document.querySelector('.add_ingredient_plus');
+    navToAddIngredientButton.addEventListener('click', () => {
+        app.innerHTML = AddIngredientPage();
+    })
+}
+
+function AddIngredientToParent() {
+    app.addEventListener('click', (event) => {
+        if(event.target.classList.contains('add_ingredient_submit')) {
+            const ingredient = event.target.parentElement.querySelector('#add_ingredient_name').value;
+            apiActions.postRequest('http://localhost:8080/parents/89/add-ingredient', {
+                "ingredient": ingredient
+            }, (parents) => {
+                app.innerHTML = ParentPage(parents);
+                navToDeleteChildPage();
+                navToAddChildPage();
+                navToAddIngredientPage();
+            })
+        }
+    })
+}
---
+++ client-side/js/pages/AddIngredientPage.js
@@ -0,0 +1,8 @@
+export default function AddIngredientPage() {
+    return `
+        <section class="add_ingredient">
+            <input class="test5" id="add_ingredient_name" type="text" placeholder="Ingredient Name"/>
+            <button class="add_ingredient_submit" type="button">Save</button>
+        </section>
+    `;
+}
