2a5b2c5d9f043b4195b17ba8d4188beb9bac7199
shuaybfarah <shuaybfarah18@gmail.com>
2021-08-19 22:32:49 -0400

    took out big profile background image

--- client-side/js/pages/ParentPage.js
+++ client-side/js/pages/ParentPage.js
@@ -2,10 +2,6 @@ import Parent from '../components/Parent.js';
 
 export default function ParentPage(parent) {
     return `
-    <div class="profile__page_header">
-        <div id="profile__header_image" class="header__box"></div>
-        <h2 class="your__profile_header">Your Profile</h2>
-    </div>
         <div class="user_info">
             ${Parent(parent)}
         </div>
